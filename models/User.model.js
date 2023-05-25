225a85fc87ad6c4c8bfa1db47073f5b37902d1c7
Miriam Hamzaoui <mimi.d3v@gmail.com>
2023-05-25 23:44:12 +0200

    done

--- models/User.model.js
+++ models/User.model.js
@@ -1,14 +1,33 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const Schema = mongoose.Schema;
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
   username: {
     type: String,
-    unique: true
+    unique: true,
   },
-  password: String
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-const User = model("User", userSchema);
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
 
-module.exports = User;
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+module.exports = model("User", userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
-const router = require("express").Router();
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("index"); // Render the index view (homepage)
 });
 
+router.get("/signup", (req, res, next) => {
+  res.render("signup"); // Render the signup view
+});
+
+router.post("/signup", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.create({ username, password });
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
